8c2bf7778a18403c4a26c333c4bac570e965581b
Hatem Alshafaei <halshafaei@gmail.com>
2018-04-30 15:42:26 +0200

    working sign-in + mqtt

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var mqtt = require('mqtt');
 var router = express.Router();
 var url = require('url');
+var User=require('../lib/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,4 +10,34 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res) {
+  
+  var userid= req.body.userid;
+  var pswrd= req.body.pswrd;
+  console.log(userid);
+  console.log(pswrd);
+
+  
+  User.findOne({username:userid,password:pswrd}, function(err,user){
+    if(err){
+      console.log(err);
+      return res.status(500).send();
+      
+    }
+
+    console.log(user);
+    if(!user)
+      return res.redirect("/");
+    // return res.status(200).send();
+    res.redirect('index')
+
+    
+  
+
+  }); 
+    
+  
+  
+});
+  
 module.exports = router;
